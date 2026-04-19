090f9ecb9bbd574a72a5dad491d490743a702471
Toavina <thleong3@gmail.com>
2026-04-20 02:26:53 +0300

    component wise ESS

--- src/mcmc/utils.py
+++ src/mcmc/utils.py
@@ -44,7 +44,7 @@ def __periodigram_mess(samples: jax.Array) -> float:
 
     # Compute covariance matrix
     covar = jnp.cov(samples, rowvar=False)
-    det_covar = jnp.linalg.det(covar + 1e-10 * jnp.eye(d))
+    det_covar = jnp.linalg.det(covar)
 
     # Compute periodigram
     samples_centered = samples - samples.mean(axis=0)
@@ -58,7 +58,7 @@ def __periodigram_mess(samples: jax.Array) -> float:
     indices = jnp.arange(-m, m + 1) % n
     psd_at_zero = jnp.sum(periodigram[indices] * weights[:, None, None], axis=0)
 
-    det_sigma = jnp.linalg.det(psd_at_zero.real + 1e-10 * jnp.eye(d))
+    det_sigma = jnp.linalg.det(psd_at_zero.real)
 
     return n * (det_covar / det_sigma) ** (1 / d)
 
@@ -74,17 +74,17 @@ def __batchmeans_mess(samples: jax.Array) -> float:
       https://doi.org/10.1093/biomet/asz002
     """
 
-    n, p = samples.shape
+    n, d = samples.shape
 
     # Compute covariance matrix
     covar = jnp.cov(samples, rowvar=False)
-    det_covar = jnp.linalg.det(covar + 1e-10 * jnp.eye(p))
+    det_covar = jnp.linalg.det(covar)
 
-    # Reshape to (num_batches, batch_size, p)
+    # Reshape to (num_batches, batch_size, d)
     batch_size = jnp.floor(jnp.sqrt(n)).astype(int)
     num_batches = n // batch_size
     reshaped_samples = samples[: num_batches * batch_size].reshape(
-        num_batches, batch_size, p
+        num_batches, batch_size, d
     )
 
     # Calculate means of each batch
@@ -94,6 +94,90 @@ def __batchmeans_mess(samples: jax.Array) -> float:
     # Calculate Sigma: The variance of the batch means scaled by batch size
     diff = batch_means - overall_mean
     sigma_mat = (batch_size / (num_batches - 1)) * (diff.T @ diff)
-    det_sigma = jnp.linalg.det(sigma_mat + 1e-10 * jnp.eye(p))
+    det_sigma = jnp.linalg.det(sigma_mat)
 
-    return n * (det_covar / det_sigma) ** (1 / p)
+    return n * (det_covar / det_sigma) ** (1 / d)
+
+
+def ess(
+    samples: jax.Array, method: Literal["periodigram", "batchmeans"] = "periodigram"
+) -> float:
+    """
+    Compute the component-wise/univariate Effective Sample Size
+
+    :Parameters
+        - samples: samples from a monte carlo sampler
+        - method: `periodigram` or `batchmeans`
+
+    :Returns
+        - ess: component-wise/univariate Effective Sample Size
+    """
+
+    if samples.ndim != 1 and samples.ndim != 2:
+        raise ValueError("The chain should be 1D or 2D.")
+
+    if samples.ndim == 1:
+        samples = samples.reshape(-1, 1)
+
+    match method:
+        case "periodigram":
+            return __periodigram_ess(samples)
+
+        case "batchmeans":
+            return __batchmeans_ess(samples)
+
+        case _:
+            raise ValueError("Unknown method")
+
+
+def __periodigram_ess(samples: jax.Array) -> float:
+    """
+    Compute the univariate Effective Sample Size using periodigram
+    """
+
+    n, d = samples.shape
+
+    # Component-wise variance
+    var = jnp.var(samples, axis=0)
+
+    # Compute the auto-covariance sum
+    samples_centered = samples - samples.mean(axis=0)
+    samples_fft = jnp.fft.fft(samples_centered, axis=0)
+    power_spec = jnp.abs(samples_fft) ** 2 / n
+
+    # Variance estimation smoothing (Bartlett window)
+    m = jnp.floor(jnp.sqrt(n)).astype(int)
+    weights = 1 - jnp.abs(jnp.arange(-m, m + 1)) / (m + 1)
+    weights = weights / weights.sum()
+    indices = jnp.arange(-m, m + 1) % n
+    psd_at_zero = jnp.sum(power_spec[indices] * weights[:, None], axis=0)
+
+    return n * var / psd_at_zero
+
+
+def __batchmeans_ess(samples: jax.Array) -> float:
+    """
+    Compute the univariate Effective Sample Size using batch means
+    """
+
+    n, d = samples.shape
+
+    # Component-wise variance
+    var = jnp.var(samples, axis=0)
+
+    # Reshape to (num_batches, batch_size, d)
+    batch_size = jnp.floor(jnp.sqrt(n)).astype(int)
+    num_batches = n // batch_size
+    reshaped_samples = samples[: num_batches * batch_size].reshape(
+        num_batches, batch_size, d
+    )
+
+    # Calculate means of each batch
+    batch_means = jnp.mean(reshaped_samples, axis=1)
+    overall_mean = jnp.mean(samples, axis=0)
+
+    # Calculate Sigma: The variance of the batch means scaled by batch size
+    diff = batch_means - overall_mean
+    sigma = (batch_size / (num_batches - 1)) * jnp.sum(diff**2, axis=0)
+
+    return n * var / sigma
